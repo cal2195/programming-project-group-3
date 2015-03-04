7d0222bb66f8d1df650a5c46fc3ab3a8fb2d8c20
Cal Martin <cmartin7@tcd.ie>
2015-03-04 15:15:30 +0000

    Initial Upload!

---
+++ ProgrammingProject/src/programmingproject/ProgrammingProject.java
@@ -0,0 +1,22 @@
+package programmingproject;
+
+import processing.core.PApplet;
+
+/**
+ *
+ * @author cal
+ */
+public class ProgrammingProject extends PApplet
+{
+    @Override
+    public void setup()
+    {
+        
+    }
+    
+    @Override
+    public void draw()
+    {
+        
+    }
+}
