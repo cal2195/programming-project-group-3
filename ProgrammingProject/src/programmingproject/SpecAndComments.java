d145cbe3e78601205377fef7932079b8c024cb2e
dcrawfordtcd <crawford@tcd.ie>
2015-03-09 21:40:56 +0000

    Updated "SpecAndComments" with project specification details that are available
    Former-commit-id: a3ff75730da64106da89a97b3526203494683009

--- ProgrammingProject/src/programmingproject/SpecAndComments.java
+++ ProgrammingProject/src/programmingproject/SpecAndComments.java
@@ -7,14 +7,37 @@ package programmingproject;
 public class SpecAndComments {
     
 }
+//N.B. contains minimum spec stuff we need to implement
 /*
-2. Structure
-The program will contain the following components:
-- Code to read in the data from a file and load it into memory.
+• read in the csv file and store the data (in
+setup), as for last week.
+• create a (simple) query result and set this
+to be the current query (in setup)
+• write a method which will draw this query
+result as a chart (e.g., bar chart).
+• draw the results of this query as a bar
+chart (in draw), using the method you have
+defined.
+
+o Not all the data will be shown on the screen at one time, and so a set of queries must be defined in your
+code. At a minimum, the following queries should be implemented:
+▪ View the data for a particular medallion.
+▪ Data for a particular vendor.
+▪ Summary data for a rate_code.
+▪ Identify “hot”areas of the city. Hot areas are identified by the affluence of pick-ups and drop-offs
+at different times of the day (e.g., many drop-offs in the business area).
+▪ Statistic (longest, shortest, average trip with respect to time and distance) information for all trips
+of the selected data in the aforementioned categories.
 
 
-- Code to select a subset of this data.
-N.B - SUBSET - we don't have to load the whole thing. Hence, multiple loadData methods to grab the relevant data
-(or store a representative sample?)
+*/
+
+
+//Design decisions or clever stuff we did (Cardozo said to stick anything like this into our reports)
+
+/*
+Exceptional compression of data
+Data input handles null fields by skipping entry
+
 
 */
