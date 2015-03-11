a1ee1d31a346adbb4033b2896c84759f1db548cd
dcrawfordtcd <crawford@tcd.ie>
2015-03-11 18:40:22 +0000

    Fixed massive bugs, conversion to secs was massively off due to multiplying seconds elapsed to current month by months elapsed. I also fixed a bunch of smaller bugs, including failing at identifying the month of december and a rare bug that caused seconds to display as 60. This should now actually work, but please feed in a couple of test cases if you have the... *drumroll* ... time!
    Former-commit-id: 58eb25edb70c9d187758616bb798797e613ec839

--- ProgrammingProject/src/programmingproject/DateTime.java
+++ ProgrammingProject/src/programmingproject/DateTime.java
@@ -6,98 +6,90 @@ package programmingproject;
  */
 public class DateTime
 {
+	public static int SECONDS_PER_DAY = 86400;
+	public static int SECONDS_PER_HOUR = 3600;
+	public static final long[] SECONDS_TILL_MONTH_STARTS = {0,2678400,5097600,7776000, 10368000, 
+		13046400, 15638400, 18316800, 20995200, 23587200, 26265600, 28857600};
+	public static void main(String[] args) {
 
-    public static final int SECONDS_PER_DAY = 86400;
-    public static final int SECONDS_PER_HOUR = 3600;
-    public static final long[] SECONDS_TILL_MONTH_STARTS =
-    {
-        0, 2678400, 5097600, 7776000, 10368000,
-        13046400, 15638400, 18316800, 20995200, 23587200, 26265600, 28857600
-    };
+		String[] tests = {"2013-04-20 01:41:51", "2013-07-30 13:01:00", "2013-12-31 23:59:59"};
+		for(int i = 0; i < tests.length; i++)
+		{
+			String test = tests[i];
+			System.out.println(test);
+			long dumb = dateTimeToSecs(test);
+			System.out.println(dumb);
+			String backToString = secsToDateTime(dumb);
+			System.out.println(backToString);
+			System.out.println(" ");
+		}
+	}
 
-    public static void main(String[] args)
-    {
 
-        String[] tests =
-        {
-            "2013-01-10 23:41:51", "2013-01-07 23:54:15", "2013-01-01 14:02:01"
-        };
-        for (int i = 0; i < tests.length; i++)
-        {
-            String test = tests[i];
-            System.out.println(test);
-            long dumb = dateTimeToSecs(test);
-            System.out.println(dumb);
-            String backToString = secsToDateTime(dumb);
-            System.out.println(backToString);
-            System.out.println(" ");
-        }
-    }
+	public static long dateTimeToSecs(String dateTime){
+		long totalSeconds = 0;
 
-    public static long dateTimeToSecs(String dateTime)
-    {
-        String[] dateTimeArr = dateTime.split(" ");
-        String[] date = dateTimeArr[0].split("/");
-        String[] time = dateTimeArr[1].split(":");
+		String[] dateTimeArr = dateTime.split(" ");  		
+		String[] date = dateTimeArr[0].split("-");
+		String[] time = dateTimeArr[1].split(":");
 
-        int months = Integer.parseInt(date[1]);
-        int days = Integer.parseInt(date[2]);
-        int hours = Integer.parseInt(time[0]);
-        int minutes = Integer.parseInt(time[1]);
-        int seconds = Integer.parseInt(time[2]);
+		int months = Integer.parseInt(date[1]);
+		int days =  Integer.parseInt(date[2]);
+		int hours =  Integer.parseInt(time[0]);
+		int minutes =  Integer.parseInt(time[1]);
+		int seconds =  Integer.parseInt(time[2]);
 
-        long secondsSince2013 = months * SECONDS_TILL_MONTH_STARTS[months - 1] + days * SECONDS_PER_DAY + hours * SECONDS_PER_HOUR + minutes * 60 + seconds;
+		long secondsSince2013 = SECONDS_TILL_MONTH_STARTS[months - 1] + days*SECONDS_PER_DAY + hours*SECONDS_PER_HOUR + minutes*60 + seconds;
 
-        return secondsSince2013;
-    }
+		return secondsSince2013;
+	}
 
-    public static String secsToDateTime(long secs)
-    {
+	public static String secsToDateTime(long secs){
 
-        String dateTime = "2013-";
-        //finds month
-        int i = 0;
-        while (SECONDS_TILL_MONTH_STARTS[i] < secs)
-        {
-            i++;
-        }
-        dateTime += String.format("%02d", i);
-        dateTime += "-";
-        secs -= SECONDS_TILL_MONTH_STARTS[i - 1];
+		String dateTime = "2013-";
+		//finds month
+		int i = 0;
+		while(i < 12 && SECONDS_TILL_MONTH_STARTS[i] < secs)
+		{
+			i++;
+		}
+		dateTime += String.format("%02d", i);
+		dateTime += "-";
+		secs -= SECONDS_TILL_MONTH_STARTS[i - 1];
 
-        //finds day
-        i = 0;
-        while (i * SECONDS_PER_DAY < secs)
-        {
-            i++;
-        }
-        secs -= SECONDS_PER_DAY * (i - 1);
-        dateTime += String.format("%02d", i - 1);
-        dateTime += " ";
+		//finds day
+		i = 0;
+		while(i*SECONDS_PER_DAY <  secs)
+		{
+			i++;
+		}
+		secs -= SECONDS_PER_DAY*(i-1);
+		dateTime += String.format("%02d", i-1);
+		dateTime += " ";
 
-        //finds hours
-        i = 0;
-        while (i * SECONDS_PER_HOUR < secs)
-        {
-            i++;
-        }
-        secs -= SECONDS_PER_HOUR * (i - 1);
-        dateTime += String.format("%02d", i - 1);
-        dateTime += ":";
+		//finds hours
+		i = 0;
+		while(i*SECONDS_PER_HOUR <  secs)
+		{
+			i++;
+		}
+		secs -= SECONDS_PER_HOUR*(i-1);
+		dateTime += String.format("%02d", i-1);
+		dateTime += ":";
 
-        //finds minutes
-        i = 0;
-        while (i * 60 < secs)
-        {
-            i++;
-        }
-        secs -= 60 * (i - 1);
-        dateTime += String.format("%02d", i - 1);
-        dateTime += ":";
+		//finds minutes
+		i = 0;
+		while(i*60 <=  secs)
+		{
+			i++;
+		}
+		secs -= 60*(i-1);
+		dateTime += String.format("%02d", i-1);
+		dateTime += ":";
 
-        dateTime += String.format("%02d", secs);
+		dateTime += String.format("%02d", secs);
 
-        return dateTime;
-    }
+		return dateTime;
+	}
 
 }
