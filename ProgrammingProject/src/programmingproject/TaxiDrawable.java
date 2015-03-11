7cfe94519878ea26709809984d1c476baba03d3c
dcrawfordtcd <crawford@tcd.ie>
2015-03-11 19:23:28 +0000

    Fixed this up a little make things animate nicely and easier to scale.
    Former-commit-id: edbb46f82e07af25a952363ff33a11d15875e32b

--- ProgrammingProject/src/programmingproject/TaxiDrawable.java
+++ ProgrammingProject/src/programmingproject/TaxiDrawable.java
@@ -10,11 +10,14 @@ package programmingproject;
  * @author Dan
  */
 
+
 public class TaxiDrawable {  
     float x, y, endx, endy;
     float dx, dy;
     boolean dead;
-    
+    short deathFrames;  
+    private final short DEATHFRAMES = 10;  
+    private final short SPEEDFACTOR = 2;
     
 TaxiDrawable(float x0, float y0){
             x = Data.longToXPos(x, 1000);
@@ -31,20 +34,28 @@ TaxiDrawable(Trip trip){
             endx = Data.longToXPos(longitude, 1000);
             endy = Data.latToYPos(latitude, 600);
             
-            int timeScale = 10; //larger is faster!
-            dx = timeScale*(endx - x) / trip.time;
-            dy = timeScale*(endy - y) / trip.time;
+            dx = SPEEDFACTOR*(endx - x) / trip.time;
+            dy = SPEEDFACTOR*(endy - y) / trip.time;
 }
 
 public void draw(RenderArea renderArea){
-        renderArea.translate(x, y, 3);
+        renderArea.translate(x, y, 4);
         if(dead){
-            renderArea.fill(0, 0, 0);
+            if(deathFrames < DEATHFRAMES)
+            {
+                renderArea.fill(0, 0, 0);
+                renderArea.box(3, 3, 8);
+                deathFrames++;
+            }
+            
         }
         else{
+            renderArea.stroke(0);
             renderArea.fill(255, 240, 0);
+            renderArea.box(3, 3, 8);
+            renderArea.noStroke();
         }
-        renderArea.box(10, 5, 4);
+        
 }
 
 public void moveAndCheck(){
