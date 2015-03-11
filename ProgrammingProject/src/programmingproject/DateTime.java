ca242c96a5bc71a4c0a6771073454e16b19412a2
Cal Martin <cmartin7@tcd.ie>
2015-03-11 19:48:37 +0000

    Formatting & Merge!
    Former-commit-id: 2fad45a18d99d1c6f1c9054780482dd431182fd3

--- ProgrammingProject/src/programmingproject/DateTime.java
+++ ProgrammingProject/src/programmingproject/DateTime.java
@@ -6,90 +6,100 @@ package programmingproject;
  */
 public class DateTime
 {
-	public static int SECONDS_PER_DAY = 86400;
-	public static int SECONDS_PER_HOUR = 3600;
-	public static final long[] SECONDS_TILL_MONTH_STARTS = {0,2678400,5097600,7776000, 10368000, 
-		13046400, 15638400, 18316800, 20995200, 23587200, 26265600, 28857600};
-	public static void main(String[] args) {
 
-		String[] tests = {"2013-04-20 01:41:51", "2013-07-30 13:01:00", "2013-12-31 23:59:59"};
-		for(int i = 0; i < tests.length; i++)
-		{
-			String test = tests[i];
-			System.out.println(test);
-			long dumb = dateTimeToSecs(test);
-			System.out.println(dumb);
-			String backToString = secsToDateTime(dumb);
-			System.out.println(backToString);
-			System.out.println(" ");
-		}
-	}
+    public static int SECONDS_PER_DAY = 86400;
+    public static int SECONDS_PER_HOUR = 3600;
+    public static final long[] SECONDS_TILL_MONTH_STARTS =
+    {
+        0, 2678400, 5097600, 7776000, 10368000,
+        13046400, 15638400, 18316800, 20995200, 23587200, 26265600, 28857600
+    };
 
+    public static void main(String[] args)
+    {
 
-	public static long dateTimeToSecs(String dateTime){
-		long totalSeconds = 0;
+        String[] tests =
+        {
+            "2013-04-20 01:41:51", "2013-07-30 13:01:00", "2013-12-31 23:59:59"
+        };
+        for (int i = 0; i < tests.length; i++)
+        {
+            String test = tests[i];
+            System.out.println(test);
+            long dumb = dateTimeToSecs(test);
+            System.out.println(dumb);
+            String backToString = secsToDateTime(dumb);
+            System.out.println(backToString);
+            System.out.println(" ");
+        }
+    }
 
-		String[] dateTimeArr = dateTime.split(" ");  		
-		String[] date = dateTimeArr[0].split("-");
-		String[] time = dateTimeArr[1].split(":");
+    public static long dateTimeToSecs(String dateTime)
+    {
+        long totalSeconds = 0;
 
-		int months = Integer.parseInt(date[1]);
-		int days =  Integer.parseInt(date[2]);
-		int hours =  Integer.parseInt(time[0]);
-		int minutes =  Integer.parseInt(time[1]);
-		int seconds =  Integer.parseInt(time[2]);
+        String[] dateTimeArr = dateTime.split(" ");
+        String[] date = dateTimeArr[0].split("-");
+        String[] time = dateTimeArr[1].split(":");
 
-		long secondsSince2013 = SECONDS_TILL_MONTH_STARTS[months - 1] + days*SECONDS_PER_DAY + hours*SECONDS_PER_HOUR + minutes*60 + seconds;
+        int months = Integer.parseInt(date[1]);
+        int days = Integer.parseInt(date[2]);
+        int hours = Integer.parseInt(time[0]);
+        int minutes = Integer.parseInt(time[1]);
+        int seconds = Integer.parseInt(time[2]);
 
-		return secondsSince2013;
-	}
+        long secondsSince2013 = SECONDS_TILL_MONTH_STARTS[months - 1] + days * SECONDS_PER_DAY + hours * SECONDS_PER_HOUR + minutes * 60 + seconds;
 
-	public static String secsToDateTime(long secs){
+        return secondsSince2013;
+    }
 
-		String dateTime = "2013-";
-		//finds month
-		int i = 0;
-		while(i < 12 && SECONDS_TILL_MONTH_STARTS[i] < secs)
-		{
-			i++;
-		}
-		dateTime += String.format("%02d", i);
-		dateTime += "-";
-		secs -= SECONDS_TILL_MONTH_STARTS[i - 1];
+    public static String secsToDateTime(long secs)
+    {
 
-		//finds day
-		i = 0;
-		while(i*SECONDS_PER_DAY <  secs)
-		{
-			i++;
-		}
-		secs -= SECONDS_PER_DAY*(i-1);
-		dateTime += String.format("%02d", i-1);
-		dateTime += " ";
+        String dateTime = "2013-";
+        //finds month
+        int i = 0;
+        while (i < 12 && SECONDS_TILL_MONTH_STARTS[i] < secs)
+        {
+            i++;
+        }
+        dateTime += String.format("%02d", i);
+        dateTime += "-";
+        secs -= SECONDS_TILL_MONTH_STARTS[i - 1];
 
-		//finds hours
-		i = 0;
-		while(i*SECONDS_PER_HOUR <  secs)
-		{
-			i++;
-		}
-		secs -= SECONDS_PER_HOUR*(i-1);
-		dateTime += String.format("%02d", i-1);
-		dateTime += ":";
+        //finds day
+        i = 0;
+        while (i * SECONDS_PER_DAY < secs)
+        {
+            i++;
+        }
+        secs -= SECONDS_PER_DAY * (i - 1);
+        dateTime += String.format("%02d", i - 1);
+        dateTime += " ";
 
-		//finds minutes
-		i = 0;
-		while(i*60 <=  secs)
-		{
-			i++;
-		}
-		secs -= 60*(i-1);
-		dateTime += String.format("%02d", i-1);
-		dateTime += ":";
+        //finds hours
+        i = 0;
+        while (i * SECONDS_PER_HOUR < secs)
+        {
+            i++;
+        }
+        secs -= SECONDS_PER_HOUR * (i - 1);
+        dateTime += String.format("%02d", i - 1);
+        dateTime += ":";
 
-		dateTime += String.format("%02d", secs);
+        //finds minutes
+        i = 0;
+        while (i * 60 <= secs)
+        {
+            i++;
+        }
+        secs -= 60 * (i - 1);
+        dateTime += String.format("%02d", i - 1);
+        dateTime += ":";
 
-		return dateTime;
-	}
+        dateTime += String.format("%02d", secs);
+
+        return dateTime;
+    }
 
 }
