1dcbf00ba4992d8ea6b19bf1711b142b9e77c3bd
dcrawfordtcd <crawford@tcd.ie>
2015-03-12 16:05:18 +0000

    fixed datetime, added loop that tests every possible valid date 2013
    Merge origin/master
    
    Conflicts:
    	ProgrammingProject/src/programmingproject/Data.java
    Former-commit-id: ec7bad54bef50fee4502a6adb5de10cc49f4f43c

--- ProgrammingProject/src/programmingproject/DateTime.java
+++ ProgrammingProject/src/programmingproject/DateTime.java
@@ -1,4 +1,4 @@
-package programmingproject;
+
 
 /**
  *
@@ -9,12 +9,13 @@ public class DateTime
 
 	public static int SECONDS_PER_DAY = 86400;
 	public static int SECONDS_PER_HOUR = 3600;
+	public static int[] DAYS_IN_MONTH = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 	public static final long[] SECONDS_TILL_MONTH_STARTS = {0,2678400,5097600,7776000, 10368000, 
 		13046400, 15638400, 18316800, 20995200, 23587200, 26265600, 28857600};
 
 	public static void main(String[] args) {
-
+		/*
 		String[] tests = {"2013-04-20 01:41:51", "2013-07-30 13:01:00", "2013-12-31 23:59:59",
 				"2013-01-16 21:31:00", "2013-05-01 16:51:55", "2013-09-30 11:34:52",
 				"2013-02-28 12:11:12",  "2013-06-05 20:12:12", "2013-10-02 10:32:07",
@@ -33,6 +34,68 @@ public class DateTime
 		}
 		String backToString = secsToDateTime(86400);
 		System.out.println(backToString);
+        */        
+		
+		int year, months, days, hours, minutes, seconds;
+		year = 2013;
+		months = 1;
+		days = 1;
+		hours = 0;
+		minutes = 0;
+		seconds = 0;
+		
+		while(months < 13){
+			seconds++;
+			if(seconds == 60)
+			{
+				seconds = 0;
+				minutes++;
+				if(minutes == 60)
+				{
+					minutes = 0;
+					hours++;
+					if(hours == 24)
+					{
+						hours = 0;
+						days++;
+						if(days > DAYS_IN_MONTH[months - 1])
+						{
+							/*
+							System.out.println("" + year + "-" + String.format("%02d", months) + "-" 
+					+ String.format("%02d", days) + " " + String.format("%02d", hours) 
+					+ ":" + String.format("%02d", minutes) + ":" + String.format("%02d", seconds));
+							*/
+							days = 1;
+							months++;
+							/*
+							System.out.println("" + year + "-" + String.format("%02d", months) + "-" 
+					+ String.format("%02d", days) + " " + String.format("%02d", hours) 
+					+ ":" + String.format("%02d", minutes) + ":" + String.format("%02d", seconds));
+							System.out.println("");
+							*/
+						}
+					}
+				}
+			}
+			String date = "" + year + "-" + String.format("%02d", months) + "-" 
+					+ String.format("%02d", days) + " " + String.format("%02d", hours) 
+					+ ":" + String.format("%02d", minutes) + ":" + String.format("%02d", seconds);
+			//System.out.println(date);
+			long tempSecs = DateTime.dateTimeToSecs(date);
+			String calculatedDate = DateTime.secsToDateTime(tempSecs);
+			if(!date.equals(calculatedDate))
+			{
+				//prints any dates that differ between "real" dates and calculated datetime
+				System.out.println(date);
+				System.out.println(calculatedDate);
+				System.out.println("");
+			}
+			
+		}
+				
+
+		System.out.println("Fini!");
+		
 	}
 
 
@@ -60,7 +123,7 @@ public class DateTime
 		String dateTime = "2013-";
 		//finds month
 		int i = 0;
-		while(i < 12 && SECONDS_TILL_MONTH_STARTS[i] < secs)
+		while(i < 12 && SECONDS_TILL_MONTH_STARTS[i] <= secs)
 		{
 		//	long secondsSoFar = SECONDS_TILL_MONTH_STARTS[i];
 			i++;
