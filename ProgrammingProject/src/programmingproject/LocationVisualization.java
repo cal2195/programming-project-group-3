302fa6e57eab3d004d489e1064ffb7e6dcb15461
shanefay <faysh@tcd.ie>
2015-03-14 12:43:45 +0000

    locations now draw as boxes with option 2 in map graphs. text draws flat on x y plane
    Former-commit-id: 060a8b8a10d375e244e2f0774d43601041e2bc2b

--- ProgrammingProject/src/programmingproject/LocationVisualization.java
+++ ProgrammingProject/src/programmingproject/LocationVisualization.java
@@ -29,13 +29,13 @@ public class LocationVisualization
         this.mapGraphs = mapGraphs;
         this.renderArea = area;
         locations = new ArrayList<LocationDrawable>();
-        locations.add(new LocationDrawable((float)40.7484,(float)73.9857,"Empire State Building", mapGraphs.map)); //Empire State building
-        locations.add(new LocationDrawable((float)40.7116,(float)74.0123, "Ground Zero", mapGraphs.map)); //ground zero
-        locations.add(new LocationDrawable((float)40.7577,(float)73.9857, "Times Square", mapGraphs.map)); //Times square
-        locations.add(new LocationDrawable((float)40.7701,(float)73.9821, "Broadway", mapGraphs.map)); //Broadway
-        locations.add(new LocationDrawable((float)40.6397,(float)73.7789, "JFK Airport", mapGraphs.map)); //JFK airport
-        locations.add(new LocationDrawable((float)40.7833,(float)73.9667, "Central Park", mapGraphs.map)); //Central park
-        locations.add(new LocationDrawable((float)40.7494,(float)73.9681,"UN HQ", mapGraphs.map)); //UN HQ
+        locations.add(new LocationDrawable((float)40.7484,(float)-73.9857,"Empire State Building", mapGraphs.map)); //Empire State building
+        locations.add(new LocationDrawable((float)40.7116,(float)-74.0123, "Ground Zero", mapGraphs.map)); //ground zero
+        locations.add(new LocationDrawable((float)40.7577,(float)-73.9857, "Times Square", mapGraphs.map)); //Times square
+        locations.add(new LocationDrawable((float)40.7701,(float)-73.9821, "Broadway", mapGraphs.map)); //Broadway
+        locations.add(new LocationDrawable((float)40.6397,(float)-73.7789, "JFK Airport", mapGraphs.map)); //JFK airport
+        locations.add(new LocationDrawable((float)40.7833,(float)-73.9667, "Central Park", mapGraphs.map)); //Central park
+        locations.add(new LocationDrawable((float)40.7494,(float)-73.9681,"UN HQ", mapGraphs.map)); //UN HQ
         test = new LocationDrawable((float)40.7455, (float)73.7777, "test", mapGraphs.map);
     }
     
@@ -43,9 +43,6 @@ public class LocationVisualization
     {
         renderArea.pushStyle();
         renderArea.pushMatrix();
-
-   
-        
         for(LocationDrawable l : locations)
         {
             renderArea.pushMatrix();
@@ -56,7 +53,6 @@ public class LocationVisualization
         frames++;
         renderArea.popMatrix();
         renderArea.popStyle();
-
     } 
     
 
