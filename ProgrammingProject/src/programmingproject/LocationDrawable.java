ec6305b3642dc1dd7ca1573fbb2ecd1843749620
shanefay <faysh@tcd.ie>
2015-03-14 11:37:06 +0000

    Merge origin/master into testShane Made so changes to try to get Location stuff working with  MapGraphs
    Former-commit-id: 6e33e1005e62e5d6cf652beafdc24c406178f330

---
+++ ProgrammingProject/src/programmingproject/LocationDrawable.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programmingproject;
+
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.geo.Location;
+import de.fhpotsdam.unfolding.utils.ScreenPosition;
+
+
+/**
+ *
+ * @author Shane
+ */
+public class LocationDrawable {
+    float lat;
+    float lon;
+    float x;
+    float y;
+    String name;
+    
+    
+    public LocationDrawable(float lat, float lon, String name, UnfoldingMap map)
+    {
+        this.lat = lat;
+        this.lon = lon;
+        this.name = name;
+        ScreenPosition screenPosition = map.getScreenPosition(new Location(lat, lon));
+        x = screenPosition.x;
+        y = screenPosition.y;
+    }
+    
+    public void draw(RenderArea renderArea)
+    {
+        renderArea.translate(x, y, 4);
+        renderArea.stroke(0);
+        renderArea.fill(255, 240, 0);
+        renderArea.box(3, 3, 8);
+        renderArea.noStroke();
+        //drawPyramid(x,y,10,area);
+    }
+
+}
