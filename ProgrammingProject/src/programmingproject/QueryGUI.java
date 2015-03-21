bad9f299daa42fc8d0bb58d6fd1e46651167129e
Aran Nolan <nolanar@tcd.ie>
2015-03-21 22:56:20 +0000

    queryGUI now contains sample size selector, however is not functional yet. Visualisation now selected by dropdown-list, instead of buttons.
    Former-commit-id: 3205901f6e8a8949518747cbcced81521e4bd96f

--- ProgrammingProject/src/programmingproject/QueryGUI.java
+++ ProgrammingProject/src/programmingproject/QueryGUI.java
@@ -4,6 +4,9 @@ import controlP5.Button;
 import controlP5.CheckBox;
 import controlP5.ControlP5;
 import controlP5.Group;
+import controlP5.RadioButton;
+import controlP5.Slider;
+import processing.core.PApplet;
 
 /**
  *
@@ -11,20 +14,31 @@ import controlP5.Group;
  */
 public class QueryGUI
 {
-
+    final int height;
+    final int width;
+    
     static final String QUERY_GROUP = "query";
     
     RenderArea renderArea;
     ControlP5 cp5;
     Group queryWindow;
     Button janButton, febButton;
-    CheckBox months;
+    Slider sampleSlider;
+    RadioButton sampleRadio;
+    Button subQuery1, subQuery2;
     
+    final int SAMPLE_POS = 240;
+    
+//    CheckBox months;
+
     public QueryGUI(RenderArea renderArea, ControlP5 cp5)
     {
         this.renderArea = renderArea;
         this.cp5 = cp5;
 
+        height = renderArea.height - 20;
+        width = 300;
+        
         setup();
     }
 
@@ -33,21 +47,56 @@ public class QueryGUI
         queryWindow = cp5.addGroup(QUERY_GROUP)
                          .setLabel("Queries")
                          .setBarHeight(20)
-                         .setPosition(20, 70)
-                         .setWidth(300)
+                         .setPosition(0, 20)
+                         .setWidth(width)
                          .activateEvent(false)
-                         .setBackgroundColor(renderArea.color(255, 150))
-                         .setBackgroundHeight(renderArea.height - 140);
+                         .setBackgroundColor(renderArea.color(48, 196))
+                         .setBackgroundHeight(height);
+        
+        janButton = cp5.addButton("query1")
+                       .setCaptionLabel("Set Query 1")
+                       .setPosition(10, height - 30)
+                       .setSize(100,20)
+                       .moveTo(queryWindow);
+        febButton = cp5.addButton("query2")
+                       .setCaptionLabel("Set Query 2")
+                       .setPosition(width - 110, height - 30)
+                       .setSize(100, 20)
+                       .moveTo(queryWindow);
         
         janButton = cp5.addButton("Jan")
                        .setPosition(10,10)
                        .setSize(40,20)
-                       .setGroup(QUERY_GROUP);
+                       .moveTo(queryWindow);
         febButton = cp5.addButton("Feb")
-                       .setPosition(10,40)
+                       .setPosition(60,10)
                        .setSize(40,20)
-                       .setGroup(QUERY_GROUP);
+                       .moveTo(queryWindow);
                        
+        sampleSlider = cp5.addSlider("sampleSlider")
+                   .setCaptionLabel("Sample Size")
+                   .setPosition(10,SAMPLE_POS)
+                   .setSize(160, 20)
+                   .setRange(0,100)
+                   .setNumberOfTickMarks(101)
+                   .showTickMarks(false)
+                   .setValue(50)
+                   .setGroup(QUERY_GROUP);
+                
+        sampleRadio = cp5.addRadioButton("sampleRadio")
+                         .setSize(20,20)
+                         .setPosition(10, SAMPLE_POS + 30)
+                         .setItemsPerRow(3)
+                         .setSpacingColumn(70)
+                         .addItem("10",1)
+                         .addItem("100",2)
+                         .addItem("1k",3)
+                         .addItem("10k",4)
+                         .addItem("100k",5)
+                         .addItem("1M",6)
+                         .setGroup(QUERY_GROUP)
+                         //.activate(2)
+                         ;
         
 //        months = cp5.addCheckBox("months")
 //                .setPosition(10, 10)
