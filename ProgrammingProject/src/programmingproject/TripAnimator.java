50156f3e12e71d8fc29f833f992b08fb13d3d5dc
Cal Martin <cmartin7@tcd.ie>
2015-03-26 15:37:20 +0000

    Initial lighting added!
    Former-commit-id: feaa0932fea5cd51551c3906f2faf8b66e4cd8ce

--- ProgrammingProject/src/programmingproject/TripAnimator.java
+++ ProgrammingProject/src/programmingproject/TripAnimator.java
@@ -14,46 +14,50 @@ public class TripAnimator extends AbstractVisualisation
     public static final short MAX_SPEEDFACTOR = 1000;
     public static final short MIN_SPEEDFACTOR = 0;
     public static final short SPEEDSTEP = 1;
-    
+
                         //info from http://www.timeanddate.com/sun/usa/new-york?month=1&year=2013
-                        //1st - 10th Jan
-        int[] dawnOffsets = {0,0,0,0,0,0,0,0,0,0,0,
-                        //11th - 20th Jan
-                        1,1,1,2,2,3,3,4,4,5,
-                        //21st - 31st Jan
-                        5,6,7,7,8,9,10,11,11,12,13,
-                        //1st - 10th Feb
-                        14,15,16,17,18,19,21,22,23,24,
-                        //11th - 20th Feb
-                        25,27,28,29,30,32,33,34,36,37,
-                        //21st - 28th Feb +1 dummy value
-                        39,40,41,43,44,46,47, 58
-                        };
-    
-    
-                        //1st - 10th Jan
-        int[] sunsetOffsets = {0,0,0,1,2,3,4,5,6,7,8,
-                        //11th - 20th Jan
-                        9,10,11,13,14,15,16,17,18,19,
-                        //21st - 31st Jan
-                        21,22,23,24,25,27,28,29,30,32,33,
-                        //1st - 10th Feb
-                        34,35,36,38,39,40,41,43,44,45,
-                        //11th - 20th Feb
-                        46,48,49,50,51,52,55,56,57,
-                        //21st - 28th Feb +1 dummy value
-                        58,59,61,62,63,64,65,66, 67
-                        };
+    //1st - 10th Jan
+    int[] dawnOffsets =
+    {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        //11th - 20th Jan
+        1, 1, 1, 2, 2, 3, 3, 4, 4, 5,
+        //21st - 31st Jan
+        5, 6, 7, 7, 8, 9, 10, 11, 11, 12, 13,
+        //1st - 10th Feb
+        14, 15, 16, 17, 18, 19, 21, 22, 23, 24,
+        //11th - 20th Feb
+        25, 27, 28, 29, 30, 32, 33, 34, 36, 37,
+        //21st - 28th Feb +1 dummy value
+        39, 40, 41, 43, 44, 46, 47, 58
+    };
+
+    //1st - 10th Jan
+    int[] sunsetOffsets =
+    {
+        0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8,
+        //11th - 20th Jan
+        9, 10, 11, 13, 14, 15, 16, 17, 18, 19,
+        //21st - 31st Jan
+        21, 22, 23, 24, 25, 27, 28, 29, 30, 32, 33,
+        //1st - 10th Feb
+        34, 35, 36, 38, 39, 40, 41, 43, 44, 45,
+        //11th - 20th Feb
+        46, 48, 49, 50, 51, 52, 55, 56, 57,
+        //21st - 28th Feb +1 dummy value
+        58, 59, 61, 62, 63, 64, 65, 66, 67
+    };
 
     //have tried to keep this fairly accurate but still works best with speed factors < 100, 
     public static short speedFactor = 1;
     RenderArea renderArea;
     MapGraphs mapGraphs;
+    
+    int taxisOnScreen = 0;
 
     long lastTime = 0;
     static double delta;
-    
-    
+
     Gradient gradient;
 
     //mode 0 will show all trips in parallel through the day
@@ -61,27 +65,20 @@ public class TripAnimator extends AbstractVisualisation
     //add more modes if you want, just update max modes and it'll iterate through on pressing "m"
     public static int MODE = 1;
     private static final int MAX_MODE = 1;
-    
-
-
-        
-
 
     ArrayList<Trip> trips = new ArrayList<>();
     ArrayList<Trip> queuedTrips = new ArrayList<>();
     ArrayList<TaxiDrawable> cars = new ArrayList<>();
-    
 
     double animatorSecondsPassed = 0;
 
     public TripAnimator(RenderArea renderArea, MapGraphs mapGraphs)
     {
-        
+
         gradient = new Gradient(renderArea);
-        
-        
+
         gradient.addColor(renderArea.color(0, 17, 60));//dark blue for night
-        
+
         gradient.addColor(renderArea.color(5, 42, 87));//sunrise1
         gradient.addColor(renderArea.color(43, 65, 115));//sunrise2
         gradient.addColor(renderArea.color(181, 181, 191));//sunrise3
@@ -92,13 +89,9 @@ public class TripAnimator extends AbstractVisualisation
         gradient.addColor(renderArea.color(206, 195, 230));//sunrise4 modified
         gradient.addColor(renderArea.color(179, 209, 255));//standard sky
         gradient.addColor(renderArea.color(179, 209, 255));//standard sky
-        
-        gradient.addColor(renderArea.color(179, 209, 255));//standard sky
-        
-        
-                
 
-        
+        gradient.addColor(renderArea.color(179, 209, 255));//standard sky
+
         this.renderArea = renderArea;
         this.mapGraphs = mapGraphs;
     }
@@ -106,49 +99,51 @@ public class TripAnimator extends AbstractVisualisation
     @Override
     public void draw(PGraphics3D buffer)
     {
-        
+
         buffer.pushStyle();
         buffer.pushMatrix();
         
-        int dayIndex = (int)animatorSecondsPassed/DateTime.SECONDS_PER_DAY;
-        
+        int taxis = 0;
+
+        int dayIndex = (int) animatorSecondsPassed / DateTime.SECONDS_PER_DAY;
+
         //jan 01 sunrise at 7.20 so start at 6.50
-        double dawnStart = ((6 + 0.833333333) * DateTime.SECONDS_PER_HOUR ) - dawnOffsets[dayIndex]*60;
+        double dawnStart = ((6 + 0.833333333) * DateTime.SECONDS_PER_HOUR) - dawnOffsets[dayIndex] * 60;
         double transitionLength = 1 * DateTime.SECONDS_PER_HOUR;
         double dawnEnd = dawnStart + transitionLength;
         double eachTransitionSegmentLength = transitionLength / gradient.colors.size();
-        
+
         //jan 01 sunset at 16.40 so start at 16.10
-        double sunsetStart = ((16.0 + 0.16666666666) * DateTime.SECONDS_PER_HOUR) + sunsetOffsets[dayIndex]*60;
+        double sunsetStart = ((16.0 + 0.16666666666) * DateTime.SECONDS_PER_HOUR) + sunsetOffsets[dayIndex] * 60;
         double sunsetEnd = sunsetStart + transitionLength;
-        
-        
-        
-        
+
         double currentTime = (animatorSecondsPassed % DateTime.SECONDS_PER_DAY);
-                
+
         //print statements for dawn and sunset time current day
         /*
-        System.out.println("Dawn " + DateTime.secsToHourAndMinute((long)dawnStart + 1800));
-        System.out.println("Sunset " + DateTime.secsToHourAndMinute((long)sunsetStart + 1800));
-        */
-                
-        if(currentTime < dawnStart || currentTime > sunsetEnd){            
+         System.out.println("Dawn " + DateTime.secsToHourAndMinute((long)dawnStart + 1800));
+         System.out.println("Sunset " + DateTime.secsToHourAndMinute((long)sunsetStart + 1800));
+         */
+        if (currentTime < dawnStart || currentTime > sunsetEnd)
+        {
             this.mapGraphs.setBackground(gradient.getGradient(0));
-        }
-        else if(currentTime > dawnStart && currentTime < dawnEnd){
-            this.mapGraphs.setBackground(gradient.getGradient((float) (currentTime - dawnStart)/(float)eachTransitionSegmentLength));
-         //   System.out.println((currentTime - dawnStart)/360);
-        }
-        else if(currentTime > dawnEnd && currentTime < sunsetStart)
+            mapGraphs.setAmbientLight(gradient.getGradient(0));
+        } else if (currentTime > dawnStart && currentTime < dawnEnd)
+        {
+            this.mapGraphs.setBackground(gradient.getGradient((float) (currentTime - dawnStart) / (float) eachTransitionSegmentLength));
+            mapGraphs.setAmbientLight(gradient.getGradient((float) (currentTime - dawnStart) / (float) eachTransitionSegmentLength));
+            //   System.out.println((currentTime - dawnStart)/360);
+        } else if (currentTime > dawnEnd && currentTime < sunsetStart)
         {
             this.mapGraphs.setBackground(gradient.getGradient(gradient.colors.size()));
+            mapGraphs.setAmbientLight(gradient.getGradient(gradient.colors.size()));
+        } else if (currentTime > sunsetStart && currentTime < sunsetEnd)
+        {
+            this.mapGraphs.setBackground(gradient.getGradient((float) (gradient.colors.size() - (currentTime - sunsetStart) / (float) eachTransitionSegmentLength)));
+            mapGraphs.setAmbientLight(gradient.getGradient((float) (gradient.colors.size() - (currentTime - sunsetStart) / (float) eachTransitionSegmentLength)));
+            // System.out.println((currentTime - dawnStart)/360);
         }
-        else if(currentTime > sunsetStart && currentTime < sunsetEnd){
-            this.mapGraphs.setBackground(gradient.getGradient((float) (gradient.colors.size() -(currentTime - sunsetStart)/(float)eachTransitionSegmentLength)));
-           // System.out.println((currentTime - dawnStart)/360);
-        }
-        
+
         delta = 1;
         if (lastTime == 0)
         {
@@ -165,7 +160,8 @@ public class TripAnimator extends AbstractVisualisation
         for (TaxiDrawable car : cars)
         {
             buffer.pushMatrix();
-            car.draw(buffer, (int) animatorSecondsPassed);
+            if (car.draw(buffer, (int) animatorSecondsPassed) && isOnScreen(car.x, car.y, 4, buffer))
+                taxis++;
             car.moveAndCheck((int) animatorSecondsPassed);
             buffer.popMatrix();
         }
@@ -174,25 +170,28 @@ public class TripAnimator extends AbstractVisualisation
         buffer.fill(20);
         //buffer.textFont(this.renderArea.createFont("Calibri", 50, false));
         buffer.textSize(50);
-        if(MODE == 0)
+        if (MODE == 0)
         {
             buffer.text(DateTime.secsToHourAndMinute((int) animatorSecondsPassed), -320f, 50f, 3f);
-        }
-        else if(MODE == 1)
+        } else if (MODE == 1)
         {
             String[] dateAndTime = DateTime.secsToDateTime((int) animatorSecondsPassed).split(" ");
             buffer.text(dateAndTime[0], -350f, -10f, 3f);
-            buffer.text(dateAndTime[1], -320f, 50, 3f); 
+            buffer.text(dateAndTime[1], -320f, 50, 3f);
         }
         buffer.textSize(25);
         buffer.text(speedFactor + "x realtime", -300f, 100f, 3f);
+        
+        taxisOnScreen = taxis;
+        
+        System.out.println("Taxis On Screen: " + taxisOnScreen);
+        
         buffer.popMatrix();
         buffer.popStyle();
         if (animatorSecondsPassed >= DateTime.SECONDS_PER_DAY && MODE == 0)
         {
             animatorSecondsPassed = 0;
-        }
-        else if (animatorSecondsPassed >= DateTime.SECONDS_TILL_MONTH_STARTS[2] && MODE == 1)
+        } else if (animatorSecondsPassed >= DateTime.SECONDS_TILL_MONTH_STARTS[2] && MODE == 1)
         {
             animatorSecondsPassed = 0;
         }
@@ -204,13 +203,21 @@ public class TripAnimator extends AbstractVisualisation
         System.out.println("TRIP SIZE: " + trips.size());
     }
 
+    public boolean isOnScreen(float x, float y, float z, PGraphics3D buffer)
+    {
+        float screenX = buffer.screenX(x, y, z);
+        float screenY = buffer.screenY(x, y, z);
+
+        return (screenX >= 0 && screenX <= buffer.width && screenY >= 0 && screenY <= buffer.height);
+    }
+
     public void setData(ArrayList<Trip> data)
     {
         queuedTrips = data;
         reset();
         switchData();
     }
-    
+
     @Override
     public void reloadData()
     {
@@ -244,13 +251,14 @@ public class TripAnimator extends AbstractVisualisation
         } else if (e.getKeyCode() == KeyEvent.VK_2)
         {
             setData(renderArea.query.getTripsForMonth(1, 200000));
-        }else if (e.getKeyCode() == 77) //if m is pressed
+        } else if (e.getKeyCode() == 77) //if m is pressed
         {
             MODE++;
-            if(MODE > MAX_MODE){
+            if (MODE > MAX_MODE)
+            {
                 MODE = 0;
             }
-            
+
             //recomputes cars to allow hotswapping between modes
             cars.clear();
             for (Trip trip : trips)
@@ -263,22 +271,21 @@ public class TripAnimator extends AbstractVisualisation
             setData(renderArea.query.GIVEME500LATENIGHTTAXISPLEASE(true));
         } else if (e.getKeyCode() == KeyEvent.VK_UP)
         {
-            if(e.isShiftDown())
-                {
-                    animatorSecondsPassed += DateTime.SECONDS_PER_DAY;
-                }
-                else
-                {
-                    animatorSecondsPassed += DateTime.SECONDS_PER_HOUR;
-                }
+            if (e.isShiftDown())
+            {
+                animatorSecondsPassed += DateTime.SECONDS_PER_DAY;
+            } else
+            {
+                animatorSecondsPassed += DateTime.SECONDS_PER_HOUR;
+            }
         } else if (e.getKeyCode() == KeyEvent.VK_DOWN)
         {
             if (animatorSecondsPassed > DateTime.SECONDS_PER_HOUR)
             {
-                if(e.isShiftDown() && animatorSecondsPassed > DateTime.SECONDS_PER_DAY){
+                if (e.isShiftDown() && animatorSecondsPassed > DateTime.SECONDS_PER_DAY)
+                {
                     animatorSecondsPassed -= DateTime.SECONDS_PER_DAY;
-                }
-                else
+                } else
                 {
                     animatorSecondsPassed -= DateTime.SECONDS_PER_HOUR;
                 }
@@ -295,8 +302,7 @@ public class TripAnimator extends AbstractVisualisation
                 {
                     speedFactor += SPEEDSTEP * 10;
                 }
-            }
-            else if (speedFactor < MAX_SPEEDFACTOR)
+            } else if (speedFactor < MAX_SPEEDFACTOR)
             {
                 speedFactor += SPEEDSTEP;
             }
@@ -308,8 +314,7 @@ public class TripAnimator extends AbstractVisualisation
                 {
                     speedFactor -= SPEEDSTEP * 10;
                 }
-            }
-            else if (speedFactor > MIN_SPEEDFACTOR)
+            } else if (speedFactor > MIN_SPEEDFACTOR)
             {
                 speedFactor -= SPEEDSTEP;
             }
